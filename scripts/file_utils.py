1f174762eb9c9b5fdd4ff607d9b9d5ee1083149a
Thomas Binu <mailthomasb@gmail.com>
2017-08-10 18:14:02 -0700

    added overlay in arcgis

--- scripts/file_utils.py
+++ scripts/file_utils.py
@@ -1,5 +1,5 @@
 from shutil import rmtree
-from os import listdir, mkdir
+from os import listdir, mkdir, remove
 from os.path import join, isfile, isdir, exists
 
 
@@ -15,6 +15,17 @@ def delete_dir_helper(dir_path):
         rmtree(dir_path)
         return True
     return False
+
+
+def delete_dir_files_helper(dir_path):
+    for file in listdir(dir_path):
+        file_path = join(dir_path, file)
+        try:
+            if isfile(file_path):
+                remove(file_path)
+        except Exception as e:
+            print(e)
+
 
 
 def is_jpeg(fname):
